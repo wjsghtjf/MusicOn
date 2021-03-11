24d075a6eb327bd59fddd138f4ab437fee7063bd
wjsghtjf <37579664+wjsghtjf@users.noreply.github.com>
2021-03-11 23:32:27 +0900

    feat: add music note

---
+++ HSMusic/Assets/Scripts/CenterFrame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterFrame : MonoBehaviour
+{
+    AudioSource myAudio;
+    bool musicStart = false;
+    private void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!musicStart&&collision.CompareTag("Note"))
+        {
+            myAudio.Play();
+            musicStart = true;
+        }
+    }
+}
---
+++ HSMusic/Assets/Scripts/Manage/NoteManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int bpm = 0;
+    double currentTime = 0d;
+    public Transform tfNoteAppear = null;
+    public GameObject goNote = null;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime>=60d/bpm)
+        {
+            GameObject t_note = Instantiate(goNote, tfNoteAppear.position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform);
+            currentTime -= 60d / bpm;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Note"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
